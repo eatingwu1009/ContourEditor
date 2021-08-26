ab8b9f39017b58c2a453236bb69f2c2beba081ac
Eating Wu <114040@ntuh.gov.tw>
2021-08-26 10:04:05 +0800

    Initial commit

---
+++ UserControl1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VMS.TPS.Common.Model.API;
+
+namespace V1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public List<String> StructureList { get; set; }
+        public List<String> TypeList { get; set; }
+        public UserControl1(ScriptContext scriptContext)
+        {
+            StructureList = new List<String>();
+            foreach (var structure in scriptContext.StructureSet.Structures)
+            {
+                StructureList.Add(structure.Id);
+            }
+            TypeList = new List<String>();
+            TypeList.Add("CTV");
+            TypeList.Add("GTV");
+            TypeList.Add("PTV");
+            TypeList.Add("EXTERNAL");
+            TypeList.Add("ORGAN");
+            TypeList.Add("AVOIDANCE");
+            TypeList.Add("CAVITY");
+            TypeList.Add("CONTRAST_AGENT");
+            TypeList.Add("IRRAD_VOLUME");
+            TypeList.Add("TREATED_VOLUME");
+            TypeList.Add("FIXATION");
+            TypeList.Add("CONTROL");
+            TypeList.Add("DOSE_REGION");
+            TypeList.Add("SUPPORTt");
+
+            InitializeComponent();
+            DataContext = this;
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show("Ting Ting is so smart!");
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ting Ting is so smart!");
+        }
+
+    }
+
+}
---
+++ V1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using V1;
+
+// TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
+// TODO: Uncomment the following line if the script requires write access.
+// [assembly: ESAPIScript(IsWriteable = true)]
+
+namespace VMS.TPS
+{
+  public class Script
+  {
+    public Script()
+    {
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void Execute(ScriptContext scriptContext, Window window)
+        {
+            // TODO : Add here the code that is called when the script is launched from Eclipse.
+            UserControl1 userControl = new UserControl1(scriptContext);
+            window.Content =userControl;
+            window.Title = "ContourEditor";
+            window.Height = 480;
+            window.Width = 420;
+        }
+  }
+}
